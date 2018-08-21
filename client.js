e3f42c9012145b700f366972d0e0756e627631dc
RafaQC <rafeqc@hotmail.com>
2018-08-20 19:58:15 -0500

    Checkpoint1

--- client.js
+++ client.js
@@ -1,3 +1,31 @@
-let app = require("express")
-let bodyParser = ("body_parser")
+const mongoose = require('mongoose');
 
+let url= 'mongodb://rafa:rafa1234@ds125932.mlab.com:25932/lachona_rq288'
+
+mongoose.connect(
+    url,  
+    {useNewUrlParser: true}, 
+    () => console.log("Conexión exitosa"));
+
+let Schema = mongoose.Schema;
+let ObjectId= Schema.ObjectId;
+
+const articuloSchema = new Schema ({
+   articulo:ObjectId,
+   nombre: String,
+   precio: Number,
+   existencia: String,
+   descripcion: String,
+});
+
+
+const ticketSchema = new Schema ({
+    ticket:ObjectId,
+    RFC:String,
+    articulos: {
+        
+    },
+    subtotal: number,
+    IVA: string,
+    total: boolean,
+ });
